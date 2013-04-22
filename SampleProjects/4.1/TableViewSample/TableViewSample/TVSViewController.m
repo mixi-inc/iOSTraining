431b58965c444358374fa24fb686964475b1378c
ginrou <ginrou799@gmail.com>
2013-04-22 20:02:46 +0900

    add sample project and images for Lesson 4.1

---
+++ SampleProjects/4.1/TableViewSample/TableViewSample/TVSViewController.m
@@ -0,0 +1,51 @@
+//
+//  TVSViewController.m
+//  TableViewSample
+//
+//  Created by 武田 祐一 on 2013/04/22.
+//  Copyright (c) 2013年 武田 祐一. All rights reserved.
+//
+
+#import "TVSViewController.h"
+
+@interface TVSViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation TVSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+
+    _tableView.dataSource = self;
+    _tableView.delegate = self;
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 100;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
+        cell.textLabel.text = [NSString stringWithFormat:@"%d", indexPath.row]; // 移動させた
+    }
+
+    return cell;
+}
+
+
+@end
---
+++ SampleProjects/4.1/TableViewSample/TableViewSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableViewSample
+//
+//  Created by 武田 祐一 on 2013/04/22.
+//  Copyright (c) 2013年 武田 祐一. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TVSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TVSAppDelegate class]));
+    }
+}
