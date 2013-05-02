6233597cc6eb571fe48cbca243f6f89f6ade7360
tamotamago <denden_k_t_810@yahoo.co.jp>
2013-05-02 17:18:03 +0900

    add sample project and images for 7.1

---
+++ SampleProjects/7.1/MixiUserDefaultsSample/MixiUserDefaultsSample/MixiViewController.h
@@ -0,0 +1,13 @@
+//
+//  MixiViewController.h
+//  MixiUserDefaultsSample
+//
+//  Created by 田村 航弥 on 2013/05/02.
+//  Copyright (c) 2013年 mixi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MixiViewController : UIViewController
+
+@end
---
+++ SampleProjects/7.1/MixiUserDefaultsSample/MixiUserDefaultsSample/MixiViewController.m
@@ -0,0 +1,29 @@
+//
+//  MixiViewController.m
+//  MixiUserDefaultsSample
+//
+//  Created by 田村 航弥 on 2013/05/02.
+//  Copyright (c) 2013年 mixi. All rights reserved.
+//
+
+#import "MixiViewController.h"
+
+@interface MixiViewController ()
+
+@end
+
+@implementation MixiViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
