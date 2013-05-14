561d31a8c939c9a03ec74faf93e0ffbc46522608
tamotamago <denden_k_t_810@yahoo.co.jp>
2013-05-14 16:42:11 +0900

    add sample project for 9.1

---
+++ SampleProjects/9.1/MixiCodeDesignSample/MixiCodeDesignSample/MixiViewController.h
@@ -0,0 +1,13 @@
+//
+//  MixiViewController.h
+//  MixiCodeDesignSample
+//
+//  Created by 田村 航弥 on 2013/05/14.
+//  Copyright (c) 2013年 mixi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MixiViewController : UIViewController
+
+@end
---
+++ SampleProjects/9.1/MixiCodeDesignSample/MixiCodeDesignSample/MixiViewController.m
@@ -0,0 +1,29 @@
+//
+//  MixiViewController.m
+//  MixiCodeDesignSample
+//
+//  Created by 田村 航弥 on 2013/05/14.
+//  Copyright (c) 2013年 mixi. All rights reserved.
+//
+
+#import "MixiViewController.h"
+
+@interface MixiViewController ()
+
+@end
+
+@implementation MixiViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
