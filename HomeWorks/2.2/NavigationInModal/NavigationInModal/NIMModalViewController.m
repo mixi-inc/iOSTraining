c8b846e8349b7ab20023dc8be836bde0ff45284a
sejour <develop.sejour@gmail.com>
2014-07-01 00:30:55 +0900

    HomeWorks2.2の仕様を満たすように変更。

--- HomeWorks/2.2/NavigationInModal/NavigationInModal/NIMModalViewController.m
+++ HomeWorks/2.2/NavigationInModal/NavigationInModal/NIMModalViewController.m
@@ -31,16 +31,35 @@
     // Do any additional setup after loading the view from its nib.
 
     self.title = [NSString stringWithFormat:@"%d", self.navigationController.viewControllers.count];
-
+    
+    // 戻るボタンが切るのを防ぐ
+    self.navigationItem.leftItemsSupplementBackButton = YES;
+    
     // TODO ここに navigation bar の右上をpushするボタンを配置するコードを書く
+    UIBarButtonItem *pushButton = [[UIBarButtonItem alloc] initWithTitle:@"push" style:UIBarButtonItemStylePlain target:self action:@selector(pressPushButton:)];
+    self.navigationItem.rightBarButtonItem = pushButton;
+    
     // TODO さらに、一番先頭に戻るボタンを左に配置する
-
+    UIBarButtonItem *rootButton = [[UIBarButtonItem alloc] initWithTitle:@"root" style:UIBarButtonItemStylePlain target:self action:@selector(pressRootButton:)];
+    self.navigationItem.leftBarButtonItem = rootButton;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+// プッシュする
+- (void)pressPushButton:(id)sender
+{
+    [self.navigationController pushViewController:[[NIMModalViewController alloc] init] animated:YES];
+}
+
+// ルートに戻る
+- (void)pressRootButton:(id)sender
+{
+    [self.navigationController popToRootViewControllerAnimated:YES];
 }
 
 @end
