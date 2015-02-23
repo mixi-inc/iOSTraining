c42d17cf88ce8158cd2d0dcfef03bc43d18077cd
ginrou <ginrou799@gmail.com>
2015-02-23 18:43:09 +0900

    3.1-2の回答追加

--- SampleProjects/3.1/MixiViewSample/MixiViewSample/MixiFirstViewController.m
+++ SampleProjects/3.1/MixiViewSample/MixiViewSample/MixiFirstViewController.m
@@ -57,11 +57,19 @@
     textView.text = @"あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。\nまたそのなかでいっしょになったたくさんのひとたち、ファゼーロとロザーロ、羊飼のミーロや、顔の赤いこどもたち、地主のテーモ、山猫博士のボーガント・デストゥパーゴなど、いまこの暗い巨きな石の建物のなかで考えていると、みんなむかし風のなつかしい青い幻燈のように思われます。では、わたくしはいつかの小さなみだしをつけながら、しずかにあの年のイーハトーヴォの五月から十月までを書きつけましょう。";
     [self.view addSubview:textView];
 
+//    // 課題3.1-2 特定のviewだけ削除する
+//    for (UIView *view in self.view.subviews) {
+//        if ([view isKindOfClass:[UIImageView class]]) {
+//            [view removeFromSuperview];
+//        }
+//    }
+
 }
 
 - (void)handle:(id)sender{
 
 }
+
 
 - (void)didReceiveMemoryWarning
 {
