85438ff89ae7c9c5125a4931b806b7065755b805
ginrou <ginrou799@gmail.com>
2015-03-12 11:55:00 +0900

    AutoLayoutDrillの問題追加

---
+++ SampleProjects/AutoLayout/AutoLayoutDrill/AutoLayoutDrill/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutDrill
+//
+//  Created by yuichi.takeda on 3/12/15.
+//  Copyright (c) 2015 mixi, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
